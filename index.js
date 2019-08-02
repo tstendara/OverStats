f68b637cfe1672e4003a6b306e15eed59a7e3ba1
tyler <tstendara@gmail.com>
2019-08-01 22:51:34 -0500

    fixed overwatch api

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express')
 const path = require('path')
 const overwatch = require('overwatch-api')
 const PORT = process.env.PORT || 5000
+const parser = require('body-parser')
 
 express()
+  .use(parser())
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
