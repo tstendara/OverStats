710c7031dde83c3604236b69080ee9105a155838
tyler <tstendara@gmail.com>
2019-08-08 16:50:02 -0500

    added leaderboard

---
+++ top10Players.js
@@ -0,0 +1,57 @@
+module.exports=[
+    {
+        username: 'dream1234-2695',
+        platform: 'pc',
+        origin: 'us'
+    },
+    {
+        username: 'KSAA-21785',
+        platform: 'pc',
+        origin: 'us'
+    },
+    {
+        username: 'Bepis-2685',
+        platform: 'pc',
+        origin: 'us'
+    },
+    {
+        username: 'Gabumon-21432',
+        platform: 'pc',
+        origin: 'us'
+    },
+    {
+        username: '성필최-31842',
+        platform: 'pc',
+        origin: 'ASIA'
+    },
+    {
+        username: 'Ricky-22536',
+        platform: 'pc',
+        origin: 'us'
+    },
+    {
+        username: 'ShRedLock-11258',
+        platform: 'pc',
+        origin: 'us'
+    },
+    {
+        username: 'Rady-2531',
+        platform: 'pc',
+        origin: 'us'
+    },
+    {
+        username: 'CCJ-31737',
+        platform: 'pc',
+        origin: 'us'
+    },
+    {
+        username: 'LadiesMan217-21169',
+        platform: 'pc',
+        origin: 'us'
+    },
+]
+
+// let platform = req.body.platform;
+//     let region = req.body.origin;
+//     let tag = req.body.username;
+
